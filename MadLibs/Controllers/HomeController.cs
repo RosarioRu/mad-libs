0a46be4b4f558f4894c1b93fc7c304e1b58b8c10
RosarioRu <mariarruval@gmail.com>
2022-07-13 10:34:42 -0700

    Add commented out example route.

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -4,6 +4,10 @@ namespace MadLibs.Controllers
 {
   public class HomeController : Controller
   {
+    
+    // example route below:
+    // [Route("/")]
+    // public string Letter() { return "Our virtual postcard will go here soon!"; }
 
   }
 }
